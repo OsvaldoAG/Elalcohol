d3da6a4b34c124f8c9454eff3733ef2e68bde75e
OsvaldoAG <osvaldo_1320114000@uptecamac.edu.mx>
2022-05-08 19:46:06 -0500

    edits

---
+++ resources/views/index.blade.php
@@ -0,0 +1,179 @@
+<!DOCTYPE HTML>
+<!--
+	Massively by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>BEBIDAS</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="/css/main.css" />
+		<noscript><link rel="stylesheet" href="/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="fade-in">
+
+				<!-- Intro -->
+					<div id="intro">
+						<h1>Esto es:<br />
+						EL PARAISO DEL ALCOHOL</h1>
+						<p><img src="images/IMGC1.PNG" alt="" /></a></p>
+						<ul class="actions">
+							<li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+						</ul>
+					</div>
+
+				
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul class="links">
+							<li class="active"><a href="index.html">Tipos de bebidas</a></li>
+						</ul>
+						<ul class="icons">
+							<li><a href="https://www.facebook.com/osvaldo.avilesguzman.3" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+							<li><a href="https://www.instagram.com/osvald.ag/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="https://github.com/OsvaldoAG" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Featured Post -->
+							<article class="post featured">
+								<header class="major">
+									<span class="date"><img src="images/IMGC2.gif" alt="" /></span>
+									<h2><a href="#">Informacion:<br />
+									Tequila</a></h2>
+									<p>El tequila es una bebida de origen ancestral, que se ha producido desde el siglo XVI (Aprox 1538) y que toma el nombre de la región donde nace. Tequila es por tanto también el nombre de la ciudad que dio origen a esta agua ardiente y donde se encuentran un gran número de fábricas productoras de la bebida.</p>
+								</header>
+								<a href="#" class="image main"><img src="images/IMGC3.jpg" alt="" /></a>
+								<ul class="actions special">
+									<li><a href="#" class="button large">Regresar</a></li>
+								</ul>
+							</article>
+
+						<!-- Posts -->
+							<section class="posts">
+								<article>
+									<header>
+										<span class="date"></span>
+										<h2><a href="#">RON<br />
+										</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/IMGC4.jpg" alt="" /></a>
+									<p>El ron es una bebida alcohólica, elaborada a partir de la fermentación y destilación de la melaza o el jugo de la caña de azúcar. La mayoría de su producción se encuentra en las Américas, y concretamente el Caribe, aunque también se da en otros países como las Filipinas o la India.​</p>
+									<ul class="actions special">
+										<li><a href="" class="button">.</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date"></span>
+										<h2><a href="#">WHISKY<br />
+										</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/IMGC5.jpg" alt="" /></a>
+									<p>El whisky, whiskey, wiski​ o güisqui​ es una bebida alcohólica obtenida por la destilación de la malta fermentada de cereales como cebada, trigo, centeno y maíz, y su posterior añejamiento en barriles de madera, tradicionalmente de roble blanco.</p>
+									<ul class="actions special">
+										<li><a href="" class="button">.</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date"></span>
+										<h2><a href="#">MEZCAL<br />
+										</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/IMGC6.jpg" alt="" /></a>
+									<p>El término mezcal tiene tres acepciones, en la actualidad: En su sentido primigenio, se refiere al alimento obtenido de la cocción del tallo y de la base de las hojas de esta planta. Es el nombre común de algunas especies de maguey o agave, en México.</p>
+									<ul class="actions special">
+										<li><a href="" class="button">.</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date"></span>
+										<h2><a href="#">VINO<br />
+										</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/IMGC7.jpg" alt="" /></a>
+									<p> El vino es una bebida hecha de uva, mediante la fermentación alcohólica de su mosto o zumo.​ La fermentación se produce por la acción metabólica de levaduras, que transforman los azúcares naturales del fruto en etanol y gas en forma de dióxido de carbono.</p>
+									<ul class="actions special">
+										<li><a href="" class="button">.</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date"></span>
+										<h2><a href="#">PULQUE<br />
+										</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/IMGC8.jpg" alt="" /></a>
+									<p>El pulque es una bebida fermentada tradicional de México, cuyo origen es prehispánico y que se elabora a partir de la fermentación del maguey —popularmente conocido en México como aguamiel—, del agave o maguey, en particular de dos tipos, por esa razón ambos llamados maguey pulquero: Agave salmiana y Agave atrovirens.</p>
+									<ul class="actions special">
+										<li><a href="" class="button">.</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										<span class="date"></span>
+										<h2><a href="#">CERVEZA<br />
+										</a></h2>
+									</header>
+									<a href="#" class="image fit"><img src="images/IMGC9.jpg" alt="" /></a>
+									<p>La cerveza es una bebida alcohólica, no destilada, de sabor amargo, que se fabrica con granos de cebada germinados u otros cereales cuyo almidón se fermenta en agua con levadura y se aromatiza a menudo con lúpulo, entre otras plantas.</p>
+									<ul class="actions special">
+										<li><a href="" class="button">.</a></li>
+									</ul>
+								</article>
+							</section>
+
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<section class="split contact">
+							<section class="alt">
+								<h3>Correo de contacto</h3>
+								<p>oavilesguzman@gmail.com<br />
+								</p>
+							</section>
+							<section>
+								<h3>Celular</h3>
+								<p><a href="#">5545688300</a></p>
+							</section>
+							<section>
+								<h3>Redes Sociales</h3>
+								<ul class="icons alt">
+									<li><a href="https://www.facebook.com/osvaldo.avilesguzman.3" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+									<li><a href="https://www.instagram.com/osvald.ag/" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+									<li><a href="https://github.com/OsvaldoAG" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+								</ul>
+							</section>
+						</section>
+					</footer>
+
+				<!-- Copyright -->
+					<div id="copyright">
+						<ul><li>&copy; ExamU3</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="/js/jquery.min.js"></script>
+			<script src="/js/jquery.scrollex.min.js"></script>
+			<script src="/js/jquery.scrolly.min.js"></script>
+			<script src="/js/browser.min.js"></script>
+			<script src="/js/breakpoints.min.js"></script>
+			<script src="/js/util.js"></script>
+			<script src="/js/main.js"></script>
+
+	</body>
+</html>
